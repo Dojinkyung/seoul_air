2ecc01fd45da460130ba3e7455968d9596a23f38
Dojinkyung <ehwlsrud1@naver.com>
2022-05-28 00:42:02 +0900

    ✅ feat: data, chart, css

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,8 @@
 // import styles from './routes.module.scss'
 
-import MenuBar from '../components/Header/Header'
-
 // import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const App = () => {
-  return (
-    <div>
-      <MenuBar />
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<FirstPage />} />
-          <Route path='dashboard' element={<SecondPage />} />
-        </Routes>
-      </BrowserRouter> */}
-    </div>
-  )
+  return <div>hello</div>
 }
 
 export default App
